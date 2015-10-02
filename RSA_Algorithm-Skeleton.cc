4e504ca04c59620a818a63efbd4b5fc7856c28cd
Jonathan Christopher Jones <jjones367@deepthought19.cc.gatech.edu>
2015-10-02 00:56:03 -0400

    initial commit

---
+++ RSA_Algorithm-Skeleton.cc
@@ -0,0 +1,62 @@
+// RSA Assignment for ECE4122/6122 Fall 2015
+
+#include <iostream>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "RSA_Algorithm.h"
+
+using namespace std;
+
+// Implement the RSA_Algorithm methods here
+
+// Constructor
+RSA_Algorithm::RSA_Algorithm()
+  : rng(gmp_randinit_default)
+{
+  // get a random seed for the random number generator
+  int dr = open("/dev/random", O_RDONLY);
+  if (dr < 0)
+    {
+      cout << "Can't open /dev/random, exiting" << endl;
+      exit(0);
+    }
+  unsigned long drValue;
+  read(dr, (char*)&drValue, sizeof(drValue));
+  //cout << "drValue " << drValue << endl;
+  rng.seed(drValue);
+// No need to init n, d, or e.
+}
+
+// Fill in the remainder of the RSA_Algorithm methods
+
+
+void RSA_Algorithm::PrintND()
+{ // Do not change this, right format for the grading script
+  cout << "n " << n << " d " << d << endl;
+}
+
+void RSA_Algorithm::PrintNE()
+{ // Do not change this, right format for the grading script
+  cout << "n " << n << " e " << e << endl;
+}
+
+void RSA_Algorithm::PrintNDE()
+{ // Do not change this, right format for the grading script
+  cout << "n " << n << " d " << d << " e " << e << endl;
+}
+
+void RSA_Algorithm::PrintM(mpz_class M)
+{ // Do not change this, right format for the grading script
+  cout << "M " << M << endl;
+}
+
+void RSA_Algorithm::PrintC(mpz_class C)
+{ // Do not change this, right format for the grading script
+  cout << "C " << C << endl;
+}
+
+
+
+
