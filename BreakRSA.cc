2f827ec15864fd409c9ef2aeda294d48a1121201
Jon <jjones646@gmail.com>
2015-10-04 02:00:26 -0400

    starting work on cracking rsa cyphertext

--- BreakRSA.cc
+++ BreakRSA.cc
@@ -1,4 +1,5 @@
 // Grad student portion of the RSA assignment
+// Jonathan Jones
 // Fall 2015 ECE6122
 
 #include <iostream>
@@ -7,8 +8,19 @@
 
 using namespace std;
 
+const mpz_class factorial(const mpz_class& num)
+{
+  mpz_class n = num, f = 1;
+  while (n > 1)
+    f *= n--;
+
+  return f;
+}
+
+
 int main(int argc, char** argv)
-{ // Arguments are as follows:
+{
+  // Arguments are as follows:
   //argv[1] = n;
   //argv[2] = e;  // n and e are the public key
   //argv[3] = first 6 characters of encrypted message
@@ -22,35 +34,37 @@ int main(int argc, char** argv)
 
   // Below is an example of the BreakRSA and command line arguments:
 
-// ./BreakRSA  2966772883822367927 2642027824495698257  817537070500556663 1328829247235192134 
-// 1451942276855579785 2150743175814047358 72488230455769594 1989174916172335943 962538406513796755 
-// 1069665942590443121 72678741742252898 1379869649761557209
+  // ./BreakRSA  2966772883822367927 2642027824495698257  817537070500556663 1328829247235192134
+  // 1451942276855579785 2150743175814047358 72488230455769594 1989174916172335943 962538406513796755
+  // 1069665942590443121 72678741742252898 1379869649761557209
 
-//   The corect output from the above is:
-//   HelloTest  riley CekwkABRIZFlqmWTanyXLogFgBUENvzwHpEHRCZIKRZ
-//
-//   The broken (computed) private key for the above is 4105243553
-
-
+  //   The corect output from the above is:
+  //   HelloTest  riley CekwkABRIZFlqmWTanyXLogFgBUENvzwHpEHRCZIKRZ
+  //
+  //   The broken (computed) private key for the above is 4105243553
 
   // Our one and only RSA_Algorithm object
   RSA_Algorithm rsa;
-  
+
+  mpz_class n(atol(argv[1]));
+
+  cout << argv[1] << " factorial is " << factorial(n) << endl;
+
   // First "break" the keys by factoring n and computing d
   // Set the keys in the rsa object afer calculating d
-  rsa.n = mpz_class(argv[1]);
-  rsa.e = mpz_class(argv[2]);
-  // Set rsa.d to the calculated private key d
-  // rsa.d = mpz_class(// broken d value here) 
-  for (int i = 3; i < 13; ++i)
-    { // Decrypt each set of 6 characters
-      mpz_class c(argv[i]);
-      mpz_class m = rsa.Decrypt(c);
-      //  use the get_ui() method in mpz_class to get the lower 48 bits of the m
-      unsigned long ul = m.get_ui();
-      // Now print the 6 ascii values in variable ul.  As stated above the 6 characters
-      // are in the low order 48 bits of ui.
-    }
-  cout << endl;
+  // rsa.n = mpz_class(argv[1]);
+  // rsa.e = mpz_class(argv[2]);
+  // // Set rsa.d to the calculated private key d
+  // // rsa.d = mpz_class(// broken d value here)
+  // for (int i = 3; i < 13; ++i)
+  // { // Decrypt each set of 6 characters
+  //   mpz_class c(argv[i]);
+  //   mpz_class m = rsa.Decrypt(c);
+  //   //  use the get_ui() method in mpz_class to get the lower 48 bits of the m
+  //   unsigned long ul = m.get_ui();
+  //   // Now print the 6 ascii values in variable ul.  As stated above the 6 characters
+  //   // are in the low order 48 bits of ui.
+  // }
+  // cout << endl;
 }
 
